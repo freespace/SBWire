2028c98f0762c656fd4c6da77116bbffa2db0575
Shuning Bian <freespace@gmail.com>
2017-04-02 18:13:14 +0100

    Fixes a recursion issue with twi_stop and SBWAIT in twi.c

--- src/utility/twi.c
+++ src/utility/twi.c
@@ -40,8 +40,8 @@
 
 static volatile uint8_t twi_state;
 static volatile uint8_t twi_slarw;
-static volatile uint8_t twi_sendStop;			// should the transaction end with a stop
-static volatile uint8_t twi_inRepStart;			// in the middle of a repeated start
+static volatile uint8_t twi_sendStop;     // should the transaction end with a stop
+static volatile uint8_t twi_inRepStart;     // in the middle of a repeated start
 
 static void (*twi_onSlaveTransmit)(void);
 static void (*twi_onSlaveReceive)(uint8_t*, int);
@@ -105,7 +105,7 @@ void twi_init(void)
 {
   // initialize state
   twi_state = TWI_READY;
-  twi_sendStop = true;		// default value
+  twi_sendStop = true;    // default value
   twi_inRepStart = false;
 
   // activate internal pullups for twi.
@@ -218,9 +218,9 @@ uint8_t twi_readFrom(uint8_t address, uint8_t* data, uint8_t length, uint8_t sen
     // since the ISR is ASYNC, and we could get confused if we hit the ISR before cleaning
     // up. Also, don't enable the START interrupt. There may be one pending from the
     // repeated start that we sent ourselves, and that would really confuse things.
-    twi_inRepStart = false;			// remember, we're dealing with an ASYNC ISR
+    twi_inRepStart = false;     // remember, we're dealing with an ASYNC ISR
     SB_DOWHILE(TWCR & _BV(TWWC), 0, TWDR = twi_slarw);
-    TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE);	// enable INTs, but not START
+    TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE);  // enable INTs, but not START
   }
   else
     // send start condition
@@ -296,25 +296,25 @@ uint8_t twi_writeTo(uint8_t address, uint8_t* data, uint8_t length, uint8_t wait
     // since the ISR is ASYNC, and we could get confused if we hit the ISR before cleaning
     // up. Also, don't enable the START interrupt. There may be one pending from the
     // repeated start that we sent outselves, and that would really confuse things.
-    twi_inRepStart = false;			// remember, we're dealing with an ASYNC ISR
+    twi_inRepStart = false;     // remember, we're dealing with an ASYNC ISR
     SB_DOWHILE(TWCR & _BV(TWWC), 5,  TWDR = twi_slarw);
-    TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE);	// enable INTs, but not START
+    TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE);  // enable INTs, but not START
   }
   else
     // send start condition
-    TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE) | _BV(TWSTA);	// enable INTs
+    TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE) | _BV(TWSTA); // enable INTs
 
   // wait for write operation to complete
   SB_WAIT(wait && (TWI_MTX == twi_state), 5);
 
   if (twi_error == 0xFF)
-    return 0;	// success
+    return 0; // success
   else if (twi_error == TW_MT_SLA_NACK)
-    return 2;	// error: address send, nack received
+    return 2; // error: address send, nack received
   else if (twi_error == TW_MT_DATA_NACK)
-    return 3;	// error: data send, nack received
+    return 3; // error: data send, nack received
   else
-    return 4;	// other twi error
+    return 4; // other twi error
 }
 
 /*
@@ -384,7 +384,7 @@ void twi_reply(uint8_t ack)
   if(ack){
     TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
   }else{
-	  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT);
+    TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT);
   }
 }
 
@@ -401,7 +401,13 @@ void twi_stop(void)
 
   // wait for stop condition to be exectued on bus
   // TWINT is not set after a stop condition!
-  SB_WAIT(TWCR & _BV(TWSTO),);
+  //
+  // XXX we can't use SB_WAIT here as SB_WAIT attempts to reset TWI, calling
+  // twi_stop in the process, resulting in a recursive call.
+  twi_loopcnt = 0;
+  while (TWCR & _BV(TWSTO) && twi_loopcnt < twi_maxloops) {
+    twi_loopcnt += 1;
+  }
 
   // update twi state
   twi_state = TWI_READY;
@@ -442,16 +448,16 @@ ISR(TWI_vect)
         TWDR = twi_masterBuffer[twi_masterBufferIndex++];
         twi_reply(1);
       }else{
-	if (twi_sendStop)
+  if (twi_sendStop)
           twi_stop();
-	else {
-	  twi_inRepStart = true;	// we're gonna send the START
-	  // don't enable the interrupt. We'll generate the start, but we
-	  // avoid handling the interrupt until we're in the next transaction,
-	  // at the point where we would normally issue the start.
-	  TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
-	  twi_state = TWI_READY;
-	}
+  else {
+    twi_inRepStart = true;  // we're gonna send the START
+    // don't enable the interrupt. We'll generate the start, but we
+    // avoid handling the interrupt until we're in the next transaction,
+    // at the point where we would normally issue the start.
+    TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
+    twi_state = TWI_READY;
+  }
       }
       break;
     case TW_MT_SLA_NACK:  // address sent, nack received
@@ -482,17 +488,17 @@ ISR(TWI_vect)
     case TW_MR_DATA_NACK: // data received, nack sent
       // put final byte into buffer
       twi_masterBuffer[twi_masterBufferIndex++] = TWDR;
-	if (twi_sendStop)
+  if (twi_sendStop)
           twi_stop();
-	else {
-	  twi_inRepStart = true;	// we're gonna send the START
-	  // don't enable the interrupt. We'll generate the start, but we
-	  // avoid handling the interrupt until we're in the next transaction,
-	  // at the point where we would normally issue the start.
-	  TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
-	  twi_state = TWI_READY;
-	}
-	break;
+  else {
+    twi_inRepStart = true;  // we're gonna send the START
+    // don't enable the interrupt. We'll generate the start, but we
+    // avoid handling the interrupt until we're in the next transaction,
+    // at the point where we would normally issue the start.
+    TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
+    twi_state = TWI_READY;
+  }
+  break;
     case TW_MR_SLA_NACK: // address sent, nack received
       twi_stop();
       break;
